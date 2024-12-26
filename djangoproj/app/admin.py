33b7004b75bbc05c4ec33a7d225c78c4d9d72d9b
Davi Belo <davibelo@gmail.com>
2024-12-25 22:12:01 -0300

    creating and adding models and custom user

--- djangoproj/app/admin.py
+++ djangoproj/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import UserProfile, Article
 
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Article)
--- djangoproj/djangoproj/settings.py
+++ djangoproj/djangoproj/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.AppConfig',
+    'app.apps.AppConfig', # added app to installed django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'app.UserProfile' # using custom user model
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
