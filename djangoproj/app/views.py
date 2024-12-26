4ab4413d316479768ca8d8bfb27925c4cfdaff36
Davi Belo <davibelo@gmail.com>
2024-12-26 14:04:10 -0300

    template boiler plate

--- djangoproj/app/views.py
+++ djangoproj/app/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def home(request):
     # load django ORM - Object Relational Mapper
     articles = Article.objects.all() 
-    # render the home.html template with the articles
+    # render html passing the request, template name and context
     return render(request, 'app/home.html', {'articles': articles})
