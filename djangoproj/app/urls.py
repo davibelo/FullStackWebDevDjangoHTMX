83bb97e3b0116653c195054423537ad462536047
Davi Belo <davibelo@gmail.com>
2024-12-28 10:12:48 -0300

    views

--- djangoproj/app/urls.py
+++ djangoproj/app/urls.py
@@ -4,5 +4,9 @@ from app.views import home, ArticleCreateView, ArticleListView, ArticleUpdateVie
 
 urlpatterns = [
     path("", home, name="home"),
-    path("articles/create/", ArticleCreateView.as_view(), name="article_create"),
+    path("articles/", ArticleListView.as_view(), name="article_list"),
+    path("articles/create/", ArticleCreateView.as_view(), name="article_create"),    
+    # int:pk integer primary key
+    path("articles/<int:pk>/update/", ArticleUpdateView.as_view(), name="article_update"),
+    path("articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
 ]
