94f1bc6da75de1c09b357855cc083bb62e7e91d8
Davi Belo <davibelo@gmail.com>
2024-12-27 22:37:07 -0300

    magic way to create forms

--- djangoproj/app/urls.py
+++ djangoproj/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from app.views import home
-from app.views import create_article
+from app.views import home, ArticleCreateView
+
+
 urlpatterns = [
     path("", home, name="home"),
-    path("articles/create/", create_article, name="create_article"),
+    path("articles/create/", ArticleCreateView.as_view(), name="create_article"),
 ]
--- djangoproj/app/views.py
+++ djangoproj/app/views.py
@@ -1,7 +1,8 @@
-from app.models import Article
 from django.shortcuts import render
-from app.forms import CreateArticleForm
-from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from app.models import Article
+from django.views.generic import CreateView
+
 
 def home(request):
     # load django ORM - Object Relational Mapper
@@ -10,20 +11,9 @@ def home(request):
     return render(request, "app/home.html", {"articles": articles})
 
 
-def create_article(request):
-    if request.method == "POST":
-        form = CreateArticleForm(request.POST)
-        if form.is_valid():
-            form.data = form.cleaned_data()
-            new_article = Article(
-                title = form.data["title"],
-                status = form.data["status"],
-                content = form.data["content"],
-                word_count = form.data["word_count"],
-                twitter_post = form.data["twitter_post"],
-            )
-            new_article.save()
-            return redirect("home")
-    else:
-        form = CreateArticleForm()
-    return render(request, "app/article_create.html", {"form": form})
+class ArticleCreateView(CreateView):
+    template_name = "app/article_create.html"
+    model = Article
+    fields = ["title", "status", "content", "word_count", "twitter_post"]
+    template_name = "app/article_create.html"
+    success_url = reverse_lazy("home")
