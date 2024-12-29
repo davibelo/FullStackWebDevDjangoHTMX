830044639c9feeaf5a80da92507b5788064a3fcb
Davi Belo <davibelo@gmail.com>
2024-12-29 11:24:24 -0300

    calculate word count

--- djangoproj/app/models.py
+++ djangoproj/app/models.py
@@ -1,3 +1,4 @@
+import re
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -17,7 +18,7 @@ class UserProfile(AbstractUser):
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True, default="")
-    word_count = models.IntegerField()
+    word_count = models.IntegerField(blank=True)
     twitter_post = models.TextField(blank=True, default="")
     status = models.CharField(
         max_length=20,
@@ -26,3 +27,10 @@ class Article(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+def save(self, *args, **kwargs):  
+    text = re.sub(r"<[^>]*>", "", self.content).replace("&nbsp;", "")
+    self.word_count = len(re.findall(r"\b\w+\b", text))
+    # super() is used to call the parent class method
+    super().save(*args, **kwargs)
+    
