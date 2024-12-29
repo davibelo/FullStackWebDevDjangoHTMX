ba679043ae3308f29141a7f73c05fd6bd7d3f156
Davi Belo <davibelo@gmail.com>
2024-12-28 22:16:54 -0300

    changing urls

--- djangoproj/app/urls.py
+++ djangoproj/app/urls.py
@@ -4,8 +4,8 @@ from app.views import ArticleCreateView, ArticleListView, ArticleUpdateView, Art
 
 urlpatterns = [
     path("", ArticleListView.as_view(), name="home"),
-    path("articles/create/", ArticleCreateView.as_view(), name="article_create"),        
-    path("articles/<int:pk>/update/", ArticleUpdateView.as_view(), name="article_update"),
-    path("articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
+    path("create/", ArticleCreateView.as_view(), name="article_create"),        
+    path("<int:pk>/update/", ArticleUpdateView.as_view(), name="article_update"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
     # int:pk is integer primary key
 ]
