09eaa6cd15e4d63c8fee5622468589d2b15b1948
Davi Belo <davibelo@gmail.com>
2024-12-29 23:09:10 -0300

    urls

--- djangoproj/djangoproj/urls.py
+++ djangoproj/djangoproj/urls.py
@@ -16,14 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("articles/", include("app.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += [
-        path("__debug__/", include("debug_toolbar.urls")),
-    ]
